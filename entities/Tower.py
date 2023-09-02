3c718d26b172d660d22dd383aa7e814fbb8f1fd6
Diego Perez Giordán <diego.giordan@cencosud.com>
2023-09-02 16:52:07 -0300

    Added maintower class

---
+++ entities/Tower.py
@@ -0,0 +1,17 @@
+import pygame
+from entities.entity import Entity
+
+
+class MainTower(Entity):
+    def __init__(self, color, x, y, life_points, game):
+        super().__init__(life_points=life_points, x=x, y=y)
+        self.game = game
+        self.color = color
+        self.width = 100
+        self.height = 100
+        self.rect = pygame.Rect(x, y, self.width, self.height)
+        self.speed = 0
+
+    def draw(self):
+        pygame.draw.rect(self.game.screen, self.color,
+                         self.rect, border_radius=15)
--- game.py
+++ game.py
@@ -1,25 +1,23 @@
 # Example file showing a basic pygame "game loop"
 import pygame
-
+from entities.Tower import MainTower
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-#define your classes here
-class Player(pygame.Rect):
-    def __init__(self, x, y):
-        self.left = x
-        self.top = y
-        self.width = 100
-        self.height = 100
 
-        
+class Game:
+    def __init__(self) -> None:
+        self.screen = screen
 
-#define your objects here
-player = Player(500, 350)
-player2 = Player(40, 600)
+
+game = Game()
+
+# define your objects here
+player = MainTower("red", x=300, y=300, game=game, life_points=100)
+
 
 while running:
     # poll for events
@@ -32,12 +30,10 @@ while running:
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
-    pygame.draw.rect(screen, "blue", player)
-    pygame.draw.rect(screen, "red", player2)
-
+    player.draw()
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
-pygame.quit()   
+pygame.quit()

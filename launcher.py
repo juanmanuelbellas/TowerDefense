60ab1322ced8f5b40fc775ee9d0db0f5698518fa
Juan Manuel Bellas <juanmanuelbellas@Juan-Manuel-Bellas.local>
2023-11-24 15:41:51 -0300

    Launcher GUI first draft

--- launcher.py
+++ launcher.py
@@ -1,23 +1,75 @@
 import tkinter as tk
 
-# Create the main application window
-app = tk.Tk()
-app.title("Simple Tkinter GUI")
+class TowerDefenseLauncher(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tower Defense Launcher")
+        self.geometry("500x300")
 
-# Create and add a label to the window
-label = tk.Label(app, text="Hello, Tkinter!")
-label.pack()  # Pack the label into the window
+        # Define IntVar() for selected value
+        self.selected_value = tk.IntVar()
 
-# Create a function to be triggered when the button is clicked
+        self.create_widgets()
 
+    def create_widgets(self):
+        self.intro_label = tk.Label(self, text="Choose your side:").pack()
+        
+        self.attacker = tk.Radiobutton(
+            self,
+            text="Attacker",
+            padx=0,
+            pady=5,
+            variable=self.selected_value,
+            value=1
+        ).pack()
 
-def on_button_click():
-    label.config(text="Button clicked!")
+        self.defender = tk.Radiobutton(
+            self,
+            text="Defender",
+            padx=5,
+            pady=5,
+            variable=self.selected_value,
+            value=2
+        ).pack()
 
+        self.ip_label = tk.Label(
+            self,
+            text="IP address"
+        ).pack()
 
-# Create and add a button to the window
-button = tk.Button(app, text="Click me!", command=on_button_click)
-button.pack()  # Pack the button into the window
+        self.ip = tk.Text(
+            self,
+            height=1,
+            width=25,
+            bg="white",
+        ).pack()
+        
+        self.ip_label = tk.Label(
+            self,
+            text="Port"
+        ).pack()
 
-# Start the main event loop
-app.mainloop()
+        self.port = tk.Text(
+            self,
+            height=1,
+            width=25,
+            bg="white"
+        ).pack()
+
+        self.join = tk.Button(
+            self,
+            text="Join",
+            padx=5,
+            pady=5,
+        ).pack()
+
+        self.host = tk.Button(
+            self,
+            text="Host",
+            padx=5,
+            pady=5,
+        ).pack()
+
+if __name__ == "__main__":
+    launcher = TowerDefenseLauncher()
+    launcher.mainloop()
